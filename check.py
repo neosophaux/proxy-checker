0ee0e265bc1a5b899e315f69f93c58d2879b72f8
John Doe <76149820+neosophaux@users.noreply.github.com>
2021-06-01 21:40:14 -0700

    Add the proxy checker.

---
+++ check.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+import json
+
+runs = int(sys.argv[2])
+
+with open(sys.argv[1], 'r') as proxy_list:
+    proxies = json.loads(proxy_list.read())['proxies']
+    proxy_stats = {}
+
+    for run in range(1, runs + 1):
+        for idx, proxy in enumerate(proxies):
+            if proxy not in proxy_stats:
+                proxy_stats[proxy] = {
+                    'fails': 0
+                }
+
+            proxy_sock = socket.socket()
+            proxy_addr = proxy.split(':')
+
+            proxy_sock.settimeout(1) # needs to be fast.
+
+            try:
+                proxy_sock.connect((proxy_addr[0], int(proxy_addr[1])))
+
+                print("[ Run %d/%d #%d ] Proxy '%s' succeeded." % (run, runs, idx, proxy))
+            except (OSError, socket.timeout):
+                print("[ Run %d/%d #%d ] Proxy '%s' failed." % (run, runs, idx, proxy))
+
+                proxy_stats[proxy]['fails'] += 1
+
+                if proxy_stats[proxy]['fails'] == runs:
+                    proxies.pop(idx)
+
+            proxy_sock.close()
+
+    favored_proxies = open('favored.json', 'w')
+
+    favored_proxies.write(json.dumps(proxies, indent = 4))
+    favored_proxies.close()
